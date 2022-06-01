a2348a4774ae3db986b377496a44b6a77e8b5099
nurulannisyah <ayu23nurul@gmail.com>
2022-06-01 17:59:30 +0700

    Tugas Selesai

--- index.js
+++ index.js
@@ -21,10 +21,22 @@ const dataBelanjaan = [
 ];
 
 // boleh dimodifikasi bila ingin menggunakan deklarasi fungsi yang normal
-const listBelanjaan = null;
+const listBelanjaan = (dataBelanjaan) => {
+  let item = [];
+  for (let i = 0; i < dataBelanjaan.length; i++) {
+    item[i] = `- ${dataBelanjaan[i].nama} x ${dataBelanjaan[i].kuantitas}`;
+  }
+  return item;
+}
 
 // boleh dimodifikasi bila ingin menggunakan deklarasi fungsi yang normal
-const totalBelanjaan = null;
+const totalBelanjaan = (dataBelanjaan) => {
+  let total = 0;
+  for (let i = 0; i < dataBelanjaan.length; i++) {
+    total += dataBelanjaan[i].kuantitas * dataBelanjaan[i].harga;
+  }
+  return total;
+}
 
 // ! JANGAN DIMODIFIKASI
 const main = () => {
